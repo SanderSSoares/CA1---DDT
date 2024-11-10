86a2c3a392d3feb3240e82e5ef7b7a91eb5993e3
SanderSSoares <sanderl.santos@gmail.com>
2024-11-10 12:46:51 +0000

    Initializing the Contract Election and start the structure each candidate will have

--- contracts/DigitalVotingSystem.sol
+++ contracts/DigitalVotingSystem.sol
@@ -1,3 +1,16 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0; // Specify the version of Solidity used
+
+//This project will be based in Brazil's voting system
+//Where citizen enter the number reffered to the candidate they are voting for and confirming
+//Or they can not vote to any of the them.
+
+contract Election{
+    //Structure to store candidate details
+    struct Candidate {
+        uint id; //Unique identifier for the candidate
+        string name; //name of the candidate
+        uint voteCount; //Number of votes the candidate has received
+    }
+}
